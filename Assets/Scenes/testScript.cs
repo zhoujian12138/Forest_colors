ae66ae382c8e263f89306bbae598c8c3532584fd
zhoujian12138 <77768295+zhoujian12138@users.noreply.github.com>
2022-06-18 15:25:21 +0800

    Update testScript.cs

--- Assets/Scenes/testScript.cs
+++ Assets/Scenes/testScript.cs
@@ -7,7 +7,7 @@ public class testScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        abcd
     }
 
     // Update is called once per frame
