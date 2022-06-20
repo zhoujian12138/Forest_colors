f8f6e925f29a27bea722c0dcd14a6703d86896c9
TS <1282376794@qq.com>
2022-06-20 16:01:26 +0800

    folder

---
+++ Assets/Script/Managers/MouseManager.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+
+public class MouseManager : MonoBehaviour
+{
+
+    public Texture2D point, doorway, attack, target, arrow;
+    RaycastHit hitInfo;
+    public event Action<Vector3> OnMouseClicked;
+    public event Action<GameObject> OnEnemyClicked;
+
+    //protected override void Awake()
+    //{
+    //    base.Awake();
+    //    DontDestroyOnLoad(this);
+    //}
+
+    void Update()
+    {
+        SetCursorTexture();
+        //if (InteractWithUI()) return;
+        MouseControl();
+    }
+
+    void SetCursorTexture()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if (InteractWithUI())
+        //{
+        //    Cursor.SetCursor(point, Vector2.zero, CursorMode.Auto);
+        //    return;
+        //}
+
+        //if (Physics.Raycast(ray, out hitInfo))
+        //{
+        //    //«–ªª Û±ÍÃ˘Õº
+        //    switch (hitInfo.collider.gameObject.tag)
+        //    {
+        //        case "Ground":
+        //            Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+        //            break;
+        //        case "Enemy":
+        //            Cursor.SetCursor(attack, new Vector2(16, 16), CursorMode.Auto);
+        //            break;
+        //        case "Portal":
+        //            Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+        //            break;
+        //        case "Item":
+        //            Cursor.SetCursor(point, new Vector2(16, 16), CursorMode.Auto);
+        //            break;
+
+        //        default:
+        //            Cursor.SetCursor(arrow, new Vector2(16, 16), CursorMode.Auto);
+        //            break;
+        //    }
+        //}
+    }
+
+    void MouseControl()
+    {
+        if (Input.GetMouseButtonDown(0) && hitInfo.collider != null)
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Ground"))
+                OnMouseClicked?.Invoke(hitInfo.point);
+            //if (hitInfo.collider.gameObject.CompareTag("Enemy"))
+            //    OnEnemyClicked?.Invoke(hitInfo.collider.gameObject);
+            //if (hitInfo.collider.gameObject.CompareTag("Attackable"))
+            //    OnEnemyClicked?.Invoke(hitInfo.collider.gameObject);
+            //if (hitInfo.collider.gameObject.CompareTag("Portal"))
+            //    OnMouseClicked?.Invoke(hitInfo.point);
+            //if (hitInfo.collider.gameObject.CompareTag("Item"))
+            //    OnMouseClicked?.Invoke(hitInfo.point);
+        }
+    }
+
+    //bool InteractWithUI()
+    //{
+    //    if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
+    //    {
+    //        return true;
+    //    }
+    //    else return false;
+    //}
+}
