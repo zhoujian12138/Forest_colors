315e0ed7053244e4f170b8421ea9495e8b1bd303
TS <1282376794@qq.com>
2022-06-23 15:51:54 +0800

    CharacterData_SO

---
+++ Assets/Script/Character stats/MonoBehavior/CharacterStats.cs
@@ -0,0 +1,140 @@
+using System;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    //public event Action<int, int> UpdateHealthBarOnAttack;
+    //public CharacterData_SO templateData;
+    public CharacterData_SO characterData;
+    //public AttackData_SO attackData;
+    //private AttackData_SO baseAttackData;
+    //private RuntimeAnimatorController baseAnimator;
+
+    //[Header("Weapon")]
+    //public Transform weaponSlot;
+
+    //[HideInInspector]
+    //public bool isCritical;
+
+    //void Awake()
+    //{
+    //    if (templateData != null)
+    //        characterData = Instantiate(templateData);
+
+    //    baseAttackData = Instantiate(attackData);
+    //    baseAnimator = GetComponent<Animator>().runtimeAnimatorController;
+    //}
+
+
+    #region Read from Data_SO
+    public int MaxHealth
+    {
+        get { if (characterData != null) return characterData.maxHealth; else return 0; }
+        set { characterData.maxHealth = value; }
+    }
+
+    public int CurrentHealth
+    {
+        get { if (characterData != null) return characterData.currentHealth; else return 0; }
+        set { characterData.currentHealth = value; }
+    }
+
+    public int BaseDefence
+    {
+        get { if (characterData != null) return characterData.baseDefence; else return 0; }
+        set { characterData.baseDefence = value; }
+    }
+
+    public int CurrentDefence
+    {
+        get { if (characterData != null) return characterData.currentDefence; else return 0; }
+        set { characterData.currentDefence = value; }
+    }
+    #endregion
+
+    //#region Character Combat
+
+    //public void TakeDamage(CharacterStats attacker, CharacterStats defener)
+    //{
+    //    int damage = Mathf.Max(attacker.CurrentDamage() - defener.CurrentDefence, 0);
+    //    CurrentHealth = Mathf.Max(CurrentHealth - damage, 0);
+
+    //    if (attacker.isCritical)
+    //    {
+    //        defener.GetComponent<Animator>().SetTrigger("Hit");
+    //    }
+    //    //Update UI
+    //    UpdateHealthBarOnAttack?.Invoke(CurrentHealth, MaxHealth);
+    //    //经验update
+    //    if (CurrentHealth <= 0)
+    //        attacker.characterData.UpdateExp(characterData.killPoint);
+    //}
+
+    //public void TakeDamage(int damage, CharacterStats defener)
+    //{
+    //    int currentDamge = Mathf.Max(damage - defener.CurrentDefence, 0);
+    //    CurrentHealth = Mathf.Max(CurrentHealth - currentDamge, 0);
+    //    UpdateHealthBarOnAttack?.Invoke(CurrentHealth, MaxHealth);
+    //    if (CurrentHealth <= 0)
+    //        GameManager.Instance.playerStats.characterData.UpdateExp(characterData.killPoint);
+    //}
+
+    //private int CurrentDamage()
+    //{
+    //    float coreDamage = UnityEngine.Random.Range(attackData.minDamge, attackData.maxDamage);
+
+    //    if (isCritical)
+    //    {
+    //        coreDamage *= attackData.criticalMultiplier;
+    //        Debug.Log("暴击！" + coreDamage);
+    //    }
+    //    return (int)coreDamage;
+    //}
+
+    //#endregion
+
+    //#region Equip Weapon
+    //public void ChangeWeapon(ItemData_SO weapon)
+    //{
+    //    UnEquipWeapon();
+    //    EquipWeapon(weapon);
+    //}
+
+    //public void EquipWeapon(ItemData_SO weapon)
+    //{
+    //    if (weapon.weaponPrefab != null)
+    //        Instantiate(weapon.weaponPrefab, weaponSlot);
+
+    //    //TODO:更新属性
+    //    //TODO:切换动画
+    //    attackData.ApplyWeaponData(weapon.weaponData);
+    //    GetComponent<Animator>().runtimeAnimatorController = weapon.weaponAnimator;
+    //    // InventoryManager.Instance.UpdateStatsText(MaxHealth, attackData.minDamge, attackData.maxDamage);
+    //}
+
+    //public void UnEquipWeapon()
+    //{
+    //    if (weaponSlot.transform.childCount != 0)
+    //    {
+    //        for (int i = 0; i < weaponSlot.transform.childCount; i++)
+    //        {
+    //            Destroy(weaponSlot.transform.GetChild(i).gameObject);
+    //        }
+    //    }
+    //    attackData.ApplyWeaponData(baseAttackData);
+    //    //TODO:切换动画
+    //    GetComponent<Animator>().runtimeAnimatorController = baseAnimator;
+    //}
+    //#endregion
+
+    //#region Apply Data Change
+    //public void ApplyHealth(int amount)
+    //{
+    //    if (CurrentHealth + amount <= MaxHealth)
+    //        CurrentHealth += amount;
+    //    else
+    //        CurrentHealth = MaxHealth;
+    //}
+    //#endregion
+
+}
