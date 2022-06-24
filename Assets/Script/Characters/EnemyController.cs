883bad9876520cfc07311ac507d3d184bf131fc9
zhoujian <3239703036@qq.com>
2022-06-24 11:39:33 +0800

    随机巡逻点

--- Assets/Script/Characters/EnemyController.cs
+++ Assets/Script/Characters/EnemyController.cs
@@ -17,19 +17,43 @@ public class EnemyController : MonoBehaviour
     public bool isGuard;
     private float speed;//记录原来的速度
     private GameObject attackTarget;
+    public float lookAtTime;
+    private float remainLookAtTime;
+
+    //巡逻
+    [Header("Patrol State")]
+    public float patrolRange;
+    private Vector3 wayPoint;
+    private Vector3 guardPos;
 
     //bool配合动画
     bool isWalk;
     bool isChase;
     bool isFollow;
 
+   
+
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
         speed = agent.speed;
+        guardPos = transform.position;
+        remainLookAtTime = lookAtTime;
     }
 
+    void Start()
+    {
+        if(isGuard)
+        {
+            enemyStates = EnemyStates.GUARD;
+        }
+        else
+        {
+            enemyStates = EnemyStates.PATROL;
+            GetNewWayPoint();
+        }
+    }
     void Update()
     {
         SwitchStates();
@@ -55,6 +79,7 @@ public class EnemyController : MonoBehaviour
             case EnemyStates.GUARD:
                 break;
             case EnemyStates.PATROL:
+                PatrolAction();
                 break;
             case EnemyStates.CHASE:
                 ChaseAction();
@@ -74,7 +99,17 @@ public class EnemyController : MonoBehaviour
         {
             //TODO:拉脱回到上一个状态
             isFollow = false;
-            agent.destination = transform.position;
+            if (remainLookAtTime > 0)
+            {
+                agent.destination = transform.position;
+                remainLookAtTime -= Time.deltaTime;
+            }
+
+            else if (isGuard)
+                enemyStates = EnemyStates.GUARD;
+            else
+                enemyStates = EnemyStates.PATROL;
+            
         }
         else
         {
@@ -98,6 +133,44 @@ public class EnemyController : MonoBehaviour
         attackTarget = null;
         return false;
     }
-   
 
+    void PatrolAction()
+    {
+        isChase = false;
+        agent.speed = speed * 0.5f;
+
+        //判断是否到了随机巡逻点
+        if(Vector3.Distance(wayPoint,transform.position) <= agent.stoppingDistance)
+        {
+            isWalk = false;
+            if(remainLookAtTime > 0)
+                remainLookAtTime -= Time.deltaTime;
+            else
+               GetNewWayPoint();
+        }
+        else
+        {
+            isWalk = true;
+            agent.destination = wayPoint;
+        }
+    }
+
+    void GetNewWayPoint()
+    {
+        remainLookAtTime = lookAtTime;
+
+        float randomX = UnityEngine.Random.Range(-patrolRange, patrolRange);
+        float randomZ = UnityEngine.Random.Range(-patrolRange, patrolRange);
+
+        Vector3 randomPoint = new Vector3(guardPos.x + randomX,transform.position.y, guardPos.z + randomZ);
+        //FIXME：可能出现的问题,以下是判断随机点是不是能走过去的点
+        NavMeshHit hit;
+        wayPoint = NavMesh.SamplePosition(randomPoint,out hit,patrolRange,1)? hit.position:transform.position;
+    }
+    //选中物体时才会画出它的训练范围
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, sightRadius);
+    }
 }
