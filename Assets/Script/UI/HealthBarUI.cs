c09aa130f05ddd839efe5b290af038f75add742b
DESKTOP-FO3ISM2\29867 <2986742924@qq.com>
2022-06-28 09:41:39 +0800

    还是没改明白的脚本，不知道别的电脑会不会报错

--- Assets/Script/UI/HealthBarUI.cs
+++ Assets/Script/UI/HealthBarUI.cs
@@ -20,11 +20,7 @@ public class HealthBarUI : MonoBehaviour
         currentStats = GetComponent<CharacterStats>();
 
         currentStats.UpdateHealthBarOnAttack += UpdateHealthBar;
-    }
 
-
-    void OnEnable()
-    {
         cam = Camera.main.transform;
 
         foreach(Canvas canvas in FindObjectsOfType<Canvas>())
@@ -38,19 +34,24 @@ public class HealthBarUI : MonoBehaviour
         }
     }
 
+
+    void OnEnable()
+    {
+
+    }
+
     private void UpdateHealthBar (int currentHealth, int maxHealth)
     {
-        if (currentHealth!=null && maxHealth!=null)
-        {
-            if(currentHealth <= 0)
-                Destroy(UIbar.gameObject);
 
-            UIbar.gameObject.SetActive(true);
-            timeLeft = visibleTime;
+        if(currentHealth <= 0)
+            Destroy(UIbar.gameObject);
 
-            float sliderPercent = (float)currentHealth / maxHealth;
-            healthSlider.fillAmount = sliderPercent;
-        }
+        UIbar.gameObject.SetActive(true);
+        timeLeft = visibleTime;
+
+        float sliderPercent = (float)currentHealth / maxHealth;
+        healthSlider.fillAmount = sliderPercent;
+
     }
 
     void LateUpdate()
