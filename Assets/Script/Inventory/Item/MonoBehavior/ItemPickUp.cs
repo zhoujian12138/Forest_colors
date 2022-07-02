16e3ca165fbb4d0a8084b572e008509dce7fe3a1
TS <1282376794@qq.com>
2022-07-02 10:09:07 +0800

    背包数据

--- Assets/Script/Inventory/Item/MonoBehavior/ItemPickUp.cs
+++ Assets/Script/Inventory/Item/MonoBehavior/ItemPickUp.cs
@@ -10,8 +10,10 @@ public class ItemPickUp : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
+            //将物品添加到背包
+            InventoryManager.Instance.inventoryData.AddItem(itemData, itemData.itemAmount);
             //装备武器
-            GameManager.Instance.playerStats.EquipWeapon(itemData);
+            //GameManager.Instance.playerStats.EquipWeapon(itemData);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Script/Inventory/Logic/MonoBehavior/InventoryManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : Singleton<InventoryManager>
+{
+    [Header("Inventory Data")]
+    public InventoryData_SO inventoryData;
+}
