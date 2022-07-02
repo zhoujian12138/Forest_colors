84f9973d17bae7fb2af316a86fc50d125dc8c294
zhoujian <3239703036@qq.com>
2022-07-02 09:55:17 +0800

    调整创建世界地图上的物品

--- Assets/Script/Inventory/Item/ScriptableObject/ItemData_SO.cs
+++ Assets/Script/Inventory/Item/ScriptableObject/ItemData_SO.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ItemType { Useable,Weapon,Armor}
-[CreateAssetMenu(fileName ="New Item",menuName = "Inventory/Item Data")]
+public enum ItemType { Useable, Weapon, Armor }
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item Data")]
 public class ItemData_SO : ScriptableObject
 {
     public ItemType itemType;
     public string itemName;
     public Sprite itemIcon;
     public int itemAmount;
+
+    [TextArea]
     public string description = "";
     public bool stackable;
 
