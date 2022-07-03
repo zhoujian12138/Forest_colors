4981b7fc5274f856b93ddc2d3557cf6b1d62bdfe
zhoujian <3239703036@qq.com>
2022-07-03 10:58:20 +0800

    实现背包交互物品和拖拽物品

--- Assets/Script/Inventory/Logic/MonoBehavior/InventoryManager.cs
+++ Assets/Script/Inventory/Logic/MonoBehavior/InventoryManager.cs
@@ -4,14 +4,73 @@ using UnityEngine;
 
 public class InventoryManager : Singleton<InventoryManager>
 {
+    public class DragData
+    {
+        public SlotHolder originalHolder;
+        public RectTransform originalParent;
+    }
+
     [Header("Inventory Data")]
     public InventoryData_SO inventoryData;
+    public InventoryData_SO actionData;
+    public InventoryData_SO equipmentData;
 
     [Header("ContainerS")]
     public ContainerUI inventoryUI;
+    public ContainerUI actionUI;
+    public ContainerUI equipmentUI;
+
+    [Header("Drag Canvas")]
+    public Canvas dragCanvas;
+    public DragData currentDrag;
 
     void Start()
     {
         inventoryUI.RefreshUI();
+        actionUI.RefreshUI();
+        equipmentUI.RefreshUI();
     }
+    #region 检查一个物品是否在每一个Slot范围内
+    public bool CheckInInventoryUI(Vector3 position)
+    {
+        for(int i = 0; i < inventoryUI.slotHolders.Length; i++)
+        {
+            RectTransform t = inventoryUI.slotHolders[i].transform as RectTransform;
+
+            if(RectTransformUtility.RectangleContainsScreenPoint(t,position))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool CheckInActionUI(Vector3 position)
+    {
+        for (int i = 0; i < actionUI.slotHolders.Length; i++)
+        {
+            RectTransform t = actionUI.slotHolders[i].transform as RectTransform;
+
+            if (RectTransformUtility.RectangleContainsScreenPoint(t, position))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool CheckInEquipmentUI(Vector3 position)
+    {
+        for (int i = 0; i < equipmentUI.slotHolders.Length; i++)
+        {
+            RectTransform t = equipmentUI.slotHolders[i].transform as RectTransform;
+
+            if (RectTransformUtility.RectangleContainsScreenPoint(t, position))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    #endregion
 }
