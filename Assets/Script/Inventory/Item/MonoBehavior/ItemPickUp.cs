4dc018c13ee6fdb8fec06cb6b7059842d284bf3d
zhoujian <3239703036@qq.com>
2022-07-05 00:38:22 +0800

    更新任务进度

--- Assets/Script/Inventory/Item/MonoBehavior/ItemPickUp.cs
+++ Assets/Script/Inventory/Item/MonoBehavior/ItemPickUp.cs
@@ -11,8 +11,7 @@ public class ItemPickUp : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-
-        if (other.CompareTag("Player"))
+        if(other.CompareTag("Player"))
         {
             //将物品添加到背包
             AudioManager.instance.PlaySound(getObject, transform.position);
@@ -20,8 +19,10 @@ public class ItemPickUp : MonoBehaviour
             InventoryManager.Instance.inventoryUI.RefreshUI();
             //装备武器
             //  GameManager.Instance.playerStats.EquipWeapon(itemData);
+
+            //检查是否有任务
+            QuestManager.Instance.UpdateQuestProgress(itemData.itemName, itemData.itemAmount);
             Destroy(gameObject);
         }
-    }       
-    
+    }
 }
